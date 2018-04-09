a14a26b15c7e3fb8d4c1106d90b0f283745b5765
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-04-09 13:44:41 -0700

    first commit

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <a className="instruction" href='#'>
+                {'WHAT'}
+            </a>
+            <a className="newgame" href='#'>
+                {'NEWGAME'}
+            </a>
+        </nav>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/main'
+import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+// registerServiceWorker();
