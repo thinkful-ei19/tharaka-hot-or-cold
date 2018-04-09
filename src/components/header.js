877e230fa0dd3a20f3fb8fd6fa7393cd1217764b
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-04-09 15:25:14 -0700

    did some state tests

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Nav from './nav';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div className="header">
             <Nav />
             <h1>HOT or COLD</h1>
+            {/* <div>{props.brand}</div> */}
         </div>
     )
 }
--- src/components/main.js
+++ src/components/main.js
@@ -5,8 +5,34 @@ import Game from './game';
 export default function Main() {
     return (
         <div className="main">
-            <Header />
+            <Header /> 
             <Game />
         </div>
     )
 }
+
+// export default class Main extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             brand: "Learn React"
+//         }
+//     }
+
+//     // toggleBrand = () => {
+//     //     alert('tag');
+//     //     let brand = 'Test';
+//     //     this.setState({brand})
+//     // }
+
+
+//     render() {
+//         return (
+//             <div className="main">
+//                 <Header brand={this.state.brand}/> 
+//                 <button onClick={this.toggleBrand}>Toggle Brand</button>
+//                 <Game />
+//             </div>
+//         )
+//     }
+// }
